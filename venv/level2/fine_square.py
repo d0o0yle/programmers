6d348b8dc5be6d780a5b2bbe7a6333f24d2e9d50
DOIL KIM <doili0552@gmail.com>
2021-09-08 22:07:23 +0900

    멀쩡한 사각형

--- venv/level2/fine_square.py
+++ venv/level2/fine_square.py
@@ -1,9 +1,18 @@
 def solution(w,h):
     answer = 0
-
-
-    return
+    if w == h:
+        return w * h - h
+    elif w == 1 or h == 1:
+        return 0
+    for x in range(w):
+        answer += int(x*h/w)
+    return answer * 2
 
 if __name__ == "__main__":
     w, h, result = 8, 12, 80
-    print('->', solution(w, h), result)
+    ws = [8,1,2]
+    hs = [12,10,3]
+    results = [80,0,2]
+
+    for w, h, result in zip(ws, hs, results):
+        print('->', solution(w, h), result)
